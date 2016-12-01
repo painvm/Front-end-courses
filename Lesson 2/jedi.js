2d559049f1824fe15f972100815339622895d367
painvm <jajavmemail@gmail.com>
2016-12-01 13:16:05 +0200

    Lesson 2. Linear equations
    Added support of solving the linear equations

--- Lesson 2/jedi.js
+++ Lesson 2/jedi.js
@@ -22,7 +22,8 @@ function QuadraticEquation() {
     if (form.elements.rightside.value != form.elements.rightside.value.match(template) && form.elements.rightside.value != '') {
         rightside = form.elements.rightside.value;
     }
-    a = (a == 0 ? 1 : a);
+    a = (form.elements.a.value.match(/0/) == a  ? 0 : (a == 0 ? 1 : a));
+    b = (form.elements.b.value.match(/0/) == b  ? 0 : (b == 0 ? 1 : b));
     +a;
     +b;
     +c;
@@ -30,17 +31,22 @@ function QuadraticEquation() {
 
     var solution_explain = null;
     var visibleCoefficients = {
-        a: (a > 1 ? a : (a == 1 ? '' : (a < -1 ? a : "-"))),
-        b:(b > 1 ? ' + ' + b : (b == 1 ? ' + ' : (b < -1 ? ' - ' + Math.abs(b) : " - "))),
+        a: a == 0 ? '' : (a > 1 ? a : (a == 1 ? '' : (a < -1 ? a : "-"))),
+        b: b == 0 ? '' :(b > 1 ? ' + ' + b : (b == 1 ? ' + ' : (b < -1 ? ' - ' + Math.abs(b) : " - "))),
         c:(c >= 1 ? ' + ' + c : (c == 0 ? '' : ' - ' + Math.abs(c)))
     }
-    var equationInput = (visibleCoefficients.a + "x" + "<sup>2<\/sup>" + visibleCoefficients.b + "x" + visibleCoefficients.c + ' = ' + rightside + (rightside != 0 ? "<br><br><span class='text-info'>Let's transform the equation<\/span><br><br>" + (a > 1 ? a : (a == 1 ? '' : (a < -1 ? a : "-"))) + "x" + "<sup>2<\/sup>" + (b > 1 ? ' + ' + b : (b == 1 ? ' + ' : (b < -1 ? ' - ' + Math.abs(b) : " - "))) + "x" + ((c - rightside) > 0 ? ' + ' + (c - rightside) : ((c - rightside) == 0 ? '' : " - " + Math.abs((c - rightside)))) + " = 0" : ''));
+
+    var equationInput = ((a =='' && a == 0 ? '' : visibleCoefficients.a + "x" + "<sup>2<\/sup>") + (a == 0 && b > 0 ? visibleCoefficients.b.replace(/\s\+\s/, "") : visibleCoefficients.b ) + "x" + visibleCoefficients.c + ' = ' + rightside + (rightside != 0 ? "<br><br><span class='text-info'>Let's transform the equation<\/span><br><br>" + (a =='' && a == 0 ? '' : visibleCoefficients.a + "x" + "<sup>2<\/sup>") +  (a == 0 && b > 0 ? visibleCoefficients.b.replace(/\s\+\s/, "") : visibleCoefficients.b ) + "x" + ((c - rightside) > 0 ? ' + ' + (c - rightside) : ((c - rightside) == 0 ? '' : " - " + Math.abs((c - rightside)))) + " = 0" : ''));
     document.getElementById("answer").innerHTML = equationInput;
     c -= rightside;
     var discriminant_value = (Math.pow(b, 2) - 4 * a * c);
     var discriminant =  (c != 0 ? "<span class='text-info'>Then we must calculate discriminant<\/span><br><br>"+"&#916 = "+Math.pow(b, 2) + (a * c > 0 ? " - " : " + ") + "4*" + Math.abs(a) + "*" +  Math.abs(c) + " = " +  (Math.pow(b, 2) - 4 * a * c):"<br><span class='text-info'>Break out x<\/span><br><br>"+"x("+visibleCoefficients.a+"x"+(b >=1 ? " + " : " - ")+Math.abs(b)+") = 0");
+
     document.getElementById("answer2").innerHTML = discriminant;
-    if (c == 0) {
+    if (a == 0) {
+       LinearEquation();
+    }
+    else if (c == 0) {
         solution_explain = "<br><span class='text-info'>Each of the sides is equal to zero. Here's the answer<\/span><br><br>"+"x<sub>1<\/sub>"+" = 0"+"<br>"+"x<sub>2<\/sub>"+" = " + -b/a;
         document.getElementById("answer3").innerHTML = solution_explain;
     }
@@ -58,6 +64,14 @@ function QuadraticEquation() {
     }
     function equationForPlot(arg) {
         return a*Math.pow(arg,2)+b*arg+c;
+    }
+    function LinearEquation() {
+        visibleCoefficients.b = (b > 0 ? visibleCoefficients.b.replace(/\s\+\s/, "") : visibleCoefficients.b );
+        var linear_form = "<span class='text-warning'>Oh. Looks like a linear equation. For easier solving let\'s present it like <b>bx = c</b></span><br><br>"+visibleCoefficients.b+"x = "+(-c);
+        document.getElementById("answer2").innerHTML = linear_form;
+        solution_explain = "<span class='text-info'>There is the only one root<\/span><br><br>"+ "x = "+(-c) + " &#247 "+ b + " = " + (-c/b);
+        document.getElementById("answer3").innerHTML = solution_explain;
+        document.getElementById("answer3").style.marginBottom = "5%";
     }
     var ctx = document.getElementById("scatterChart");
     var scatterChart = new Chart(ctx, {
@@ -158,5 +172,6 @@ function QuadraticEquation() {
             }
         }
     });
+
 }
 
